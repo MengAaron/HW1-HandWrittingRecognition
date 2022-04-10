1f25c32462f1aabfa2682143a83145d62c397838
MengAaron <871628893@qq.com>
2022-04-10 20:33:17 +0800

    final

--- train.py
+++ train.py
@@ -24,7 +24,7 @@ def parse_config():
     return args, easydict.EasyDict(cfg)
 
 
-def plot(x):
+def plot_mnist(x):
     x = x.reshape([28, 28])
     fig = plt.figure()
     ax1 = fig.add_subplot()
@@ -46,11 +46,23 @@ def load(parameters, path):
             parameters[name][i].data = data[name + '_' + str(i)]
 
 
-def train(net, loss_fn, optimizer, batch_size, epoch=1, loadfile=None):
+def plot_curve(iter, val):
+    fig = plt.figure(figsize=(35, 30))
+    sub = fig.add_subplot(111)
+    sub.plot(iter, val, color='orange', linewidth=1, )
+
+    plt.draw()
+    plt.show()
+
+
+def train(net, loss_fn, optimizer, batch_size, epoch=1, loadfile=False):
+    iter = []
+    loss_list = []
+    acc_list = []
     X, Y = mnist.load(type='train')
     total_it_each_epoch = X.shape[0] // batch_size
     total_pos = 0
-    if loadfile and os.path.isfile('para.npz'): load(net.parameters, 'para.npz')
+    if loadfile and os.path.isfile('para.npz'): load(net.parameters, 'para.npz') # 这里有点问题，指定一下路径比较好
     with trange(epoch) as tbar:
         for _ in tbar:
             total_pos = 0
@@ -64,28 +76,37 @@ def train(net, loss_fn, optimizer, batch_size, epoch=1, loadfile=None):
                 total_pos += acc * batch_size
                 tbar.set_description(desc='正确率为%.5f' % (total_pos / ((i + 1) * batch_size)))
                 tbar.refresh()
+                iter.append(i + _ * total_it_each_epoch)
+                loss_list.append(loss)
+                acc_list.append(total_pos / ((i + 1) * batch_size))
                 eta = loss_fn.gradient()
                 net.backward(eta)
                 optimizer.update()
-        # if i == 5000:
-        #     import pdb
-        #     pdb.set_trace()
-    save(net.parameters, 'para.npz')
+            # if i == 5000:
+            #     import pdb
+            #     pdb.set_trace()
+            save(net.parameters, 'para%d.npz' % _)
+    return iter, loss_list, acc_list
 
 
-def test(net, loss_fn, batch_size):
+def test(net, epoch=1):
     X, Y = mnist.load(type='test')
-    total_it_each_epoch = X.shape[0] // batch_size
     total_pos = 0
-    assert os.path.isfile('para.npz')
-    load(net.parameters, 'para.npz')
-    for i in tqdm(range(total_it_each_epoch)):
-        start, end = i * batch_size, (i + 1) * batch_size
-        x = X[start:end, :]
-        y = Y[start:end]
+
+    x = X
+    y = Y
+    iter = []
+    acc_list = []
+    for i in range(epoch):
+        assert os.path.isfile('para%d.npz' % i)
+        load(net.parameters, 'para%d.npz' % i)
         y_pred = net.forward(x)
-        total_pos += np.sum(y == y_pred.argmax(axis=-1))
-        print(total_pos / ((i + 1) * batch_size))
+        total_pos = np.sum(y == y_pred.argmax(axis=-1))
+        acc = total_pos / Y.shape[0]
+        iter.append(i)
+        acc_list.append(acc)
+        # print(acc)
+    return iter, acc_list
 
 
 if __name__ == '__main__':
@@ -101,5 +122,8 @@ if __name__ == '__main__':
     loss_fn = util.CELoss()
     net = util.Net(cfg.MODEL)
     optimizer = optim.SGD(cfg.lr, net.parameters, cfg.decay, cfg.lr_decay)
-    train(net, loss_fn, optimizer, batch_size, cfg.epoch, cfg.loadfile)
-    # test(net, loss_fn, batch_size)
+    iter, loss, acc = train(net, loss_fn, optimizer, batch_size, cfg.epoch, cfg.loadfile)
+    # plot_curve(iter,loss)
+    # plot_curve(iter,acc)
+    iter, acc = test(net, cfg.epoch)
+    plot_curve(iter, acc)
